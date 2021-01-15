5d800222e202f710e88f1461c70384232a00b201
CubillasCar <ccubillasolano@gmail.com>
2021-01-15 14:45:49 -0500

    Proyecto Backend listo

---
+++ Backend/src/test/java/com/gecomi/GecomiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gecomi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GecomiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
