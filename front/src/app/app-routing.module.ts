73c02ef81e98b1916c89456d27d885a469dbc86f
Dominique <dominique120@live.com>
2021-01-26 21:56:25 -0500

    Agregar frontend

---
+++ front/src/app/app-routing.module.ts
@@ -0,0 +1,42 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { PacienteComponent } from './pages/paciente/paciente.component';
+import { MedicoComponent } from './pages/medico/medico.component';
+import { PacienteEdicionComponent } from './pages/paciente/paciente-edicion/paciente-edicion.component';
+
+import { LoginComponent } from './pages/login/login.component';
+import { GuardService } from './_service/guard.service';
+import { Not403Component } from './pages/not403/not403.component';
+import { Not404Component } from './pages/not404/not404.component';
+import { RecuperarComponent } from './pages/login/recuperar/recuperar.component';
+import { TokenComponent } from './pages/login/recuperar/token/token.component';
+
+const routes: Routes = [
+  {
+    path: 'paciente', component: PacienteComponent, children: [
+      { path: 'nuevo', component: PacienteEdicionComponent },
+      { path: 'edicion/:id', component: PacienteEdicionComponent }
+    ], canActivate: [GuardService]
+  },
+  { path: 'medico', component: MedicoComponent, canActivate: [GuardService] },
+
+  {
+    path: 'recuperar', component: RecuperarComponent, children: [
+      { path: ':token', component: TokenComponent }
+    ]
+  },
+  { path: 'not-403', component: Not403Component },
+  { path: 'not-404', component: Not404Component },
+  { path: 'login', component: LoginComponent },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: '**', redirectTo: 'not-404', pathMatch: 'full' }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes, {
+    initialNavigation: 'enabled',
+    scrollPositionRestoration: 'enabled'
+  })],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
